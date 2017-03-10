a179335406916f7b779df3e8428f7b723e4cc9e0
GeorgeBai123 <xiao12312315@sina.com>
2017-03-10 13:07:54 +0800

    新建常用函数文件夹

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php 
+function demo($condition = 'email',$match,$min=6,$max=20)
+{
+	switch ($condition) {
+		case 'phone':
+			// 验证手机的操作
+			break;
+		case 'email':
+			// 验证email的操作
+			break;
+		case 'user':
+			// 验证用户的操作
+			break;
+		default:
+			# code...
+			break;
+	}
+}
