037d538c1abc774ea45403294b9b73c0de99920f
GeorgeBai123 <xiao12312315@sina.com>
2017-03-23 08:58:18 +0800

    update 3/23

--- AutoSql/phpSql.php
+++ AutoSql/phpSql.php
@@ -5,15 +5,16 @@
 require 'config.php';
 // 获取SQL文件
 $installfile = './test.sql';
-$fp = fopen($installfile, 'rb');
-$sql = fread($fp, filesize($installfile));
+$fp          = fopen($installfile, 'rb');
+$sql         = fread($fp, filesize($installfile));
 fclose($fp);
 
 // 数据库配置
 $db_charset = $db_config['db_charset'];
-$db_prefix = $db_config['db_prefix']; 
+$db_prefix  = $db_config['db_prefix']; 
+
 // 格式化sql文件
-$sql = str_replace("\r", "\n", str_replace('`'.'ts_', '`'.$db_prefix, $sql));
+$sql        = str_replace("\r", "\n", str_replace('`'.'ts_', '`'.$db_prefix, $sql));
 
 // 连接数据库
 mysql_connect($db_config['db_host'], $db_config['db_username'], $db_config['db_password']);
@@ -26,6 +27,7 @@ if (version_compare($sqlv, '5.0.0', '>')) {
 mysql_query("SET character_set_connection={$db_config['db_charset']}, character_set_results={$db_config['db_charset']}, character_set_client=binary");
 $db_charset = (strpos($db_charset, '-') === false) ? $db_charset : str_replace('-', '', $db_charset);
 mysql_query(" CREATE DATABASE IF NOT EXISTS `{$db_config['db_name']}` DEFAULT CHARACTER SET $db_charset ");
+
 // 成功与否
 if (mysql_errno()) {
     echo $errormsg = mysql_error();
@@ -34,7 +36,7 @@ if (mysql_errno()) {
 }
 
 //判断是否有用同样的数据库前缀安装过
-$re = mysql_query("SELECT COUNT(1) FROM {$db_config['db_prefix']}user");
+$re   = mysql_query("SELECT COUNT(1) FROM {$db_config['db_prefix']}user");
 $link = @mysql_fetch_row($re);
 if (intval($link[0]) > 0) {
     echo '前缀重复';
--- AutoSql/test.sql
+++ AutoSql/test.sql
@@ -23,11 +23,11 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- 表的结构 `ts_ad`
+-- 表的结构 `ts_test`
 --
 
-DROP TABLE IF EXISTS `ts_ad`;
-CREATE TABLE IF NOT EXISTS `ts_ad` (
+DROP TABLE IF EXISTS `ts_test`;
+CREATE TABLE IF NOT EXISTS `ts_test` (
   `ad_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '广告ID，主键',
   `title` varchar(255) DEFAULT NULL COMMENT '广告标题',
   `place` tinyint(1) NOT NULL DEFAULT '0' COMMENT '广告位置：0-中部；1-头部；2-左下；3-右下；4-底部；5-右上；',
@@ -42,14 +42,14 @@ CREATE TABLE IF NOT EXISTS `ts_ad` (
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COMMENT='广告位表' AUTO_INCREMENT=16 ;
 
 --
--- 插入之前先把表清空（truncate） `ts_ad`
+-- 插入之前先把表清空（truncate） `ts_test`
 --
 
-TRUNCATE TABLE `ts_ad`;
+TRUNCATE TABLE `ts_test`;
 --
--- 转存表中的数据 `ts_ad`
+-- 转存表中的数据 `ts_test`
 --
 
-INSERT INTO `ts_ad` (`ad_id`, `title`, `place`, `is_active`, `is_closable`, `ctime`, `mtime`, `display_order`, `display_type`, `content`) VALUES (11, '首页顶部广告位', 15, 1, 0, 1419321107, 1431012885, 2, 3, 'a:5:{i:0;a:2:{s:6:"banner";s:5:"43064";s:9:"bannerurl";s:86:"http://demo.thinksns.com/ts4/index.php?app=weiba&mod=Index&act=postDetail&post_id=2242";}i:1;a:2:{s:6:"banner";s:5:"42025";s:9:"bannerurl";s:86:"http://demo.thinksns.com/ts4/index.php?app=weiba&mod=Index&act=postDetail&post_id=2206";}i:2;a:2:{s:6:"banner";s:5:"43152";s:9:"bannerurl";s:86:"http://demo.thinksns.com/ts4/index.php?app=weiba&mod=Index&act=postDetail&post_id=2254";}i:3;a:2:{s:6:"banner";s:5:"42377";s:9:"bannerurl";s:86:"http://demo.thinksns.com/ts4/index.php?app=weiba&mod=Index&act=postDetail&post_id=2192";}i:4;a:2:{s:6:"banner";s:5:"42034";s:9:"bannerurl";s:86:"http://demo.thinksns.com/ts4/index.php?app=weiba&mod=Index&act=postDetail&post_id=2206";}}');
-INSERT INTO `ts_ad` (`ad_id`, `title`, `place`, `is_active`, `is_closable`, `ctime`, `mtime`, `display_order`, `display_type`, `content`) VALUES (10, '微吧首页右下', 12, 1, 0, 1419057837, 1428431626, 1, 1, '<a href="http://demo.thinksns.com/ts4/index.php?app=weiba&mod=Index&act=postDetail&post_id=2155"><a href="http://demo.thinksns.com/ts4/index.php?app=weiba&mod=Index&act=postDetail&post_id=2155"><img src="http://demo.thinksns.com/ts4/data/upload/2015/0408/00/5524010a3f84c.png" alt="" /></a></a><br />');
-INSERT INTO `ts_ad` (`ad_id`, `title`, `place`, `is_active`, `is_closable`, `ctime`, `mtime`, `display_order`, `display_type`, `content`) VALUES (14, '朋友圈右下', 3, 1, 0, 1425633186, 1432723620, 3, 1, '<p>\r\n	<a href="http://demo.thinksns.com/ts4/index.php?app=public&mod=Profile&act=index&uid=32307"></a><a href="http://demo.thinksns.com/ts4/index.php?app=public&mod=Profile&act=index&uid=32307"><img src="http://demo.thinksns.com/ts4/data/upload/2015/0527/18/5565a0a1de7ce.png" alt="" /></a> \r\n</p>');
+INSERT INTO `ts_test` (`ad_id`, `title`, `place`, `is_active`, `is_closable`, `ctime`, `mtime`, `display_order`, `display_type`, `content`) VALUES (11, '首页顶部广告位', 15, 1, 0, 1419321107, 1431012885, 2, 3, 'a:5:{i:0;a:2:{s:6:"banner";s:5:"43064";s:9:"bannerurl";s:86:"http://demo.thinksns.com/ts4/index.php?app=weiba&mod=Index&act=postDetail&post_id=2242";}i:1;a:2:{s:6:"banner";s:5:"42025";s:9:"bannerurl";s:86:"http://demo.thinksns.com/ts4/index.php?app=weiba&mod=Index&act=postDetail&post_id=2206";}i:2;a:2:{s:6:"banner";s:5:"43152";s:9:"bannerurl";s:86:"http://demo.thinksns.com/ts4/index.php?app=weiba&mod=Index&act=postDetail&post_id=2254";}i:3;a:2:{s:6:"banner";s:5:"42377";s:9:"bannerurl";s:86:"http://demo.thinksns.com/ts4/index.php?app=weiba&mod=Index&act=postDetail&post_id=2192";}i:4;a:2:{s:6:"banner";s:5:"42034";s:9:"bannerurl";s:86:"http://demo.thinksns.com/ts4/index.php?app=weiba&mod=Index&act=postDetail&post_id=2206";}}');
+INSERT INTO `ts_test` (`ad_id`, `title`, `place`, `is_active`, `is_closable`, `ctime`, `mtime`, `display_order`, `display_type`, `content`) VALUES (10, '微吧首页右下', 12, 1, 0, 1419057837, 1428431626, 1, 1, '<a href="http://demo.thinksns.com/ts4/index.php?app=weiba&mod=Index&act=postDetail&post_id=2155"><a href="http://demo.thinksns.com/ts4/index.php?app=weiba&mod=Index&act=postDetail&post_id=2155"><img src="http://demo.thinksns.com/ts4/data/upload/2015/0408/00/5524010a3f84c.png" alt="" /></a></a><br />');
+INSERT INTO `ts_test` (`ad_id`, `title`, `place`, `is_active`, `is_closable`, `ctime`, `mtime`, `display_order`, `display_type`, `content`) VALUES (14, '朋友圈右下', 3, 1, 0, 1425633186, 1432723620, 3, 1, '<p>\r\n	<a href="http://demo.thinksns.com/ts4/index.php?app=public&mod=Profile&act=index&uid=32307"></a><a href="http://demo.thinksns.com/ts4/index.php?app=public&mod=Profile&act=index&uid=32307"><img src="http://demo.thinksns.com/ts4/data/upload/2015/0527/18/5565a0a1de7ce.png" alt="" /></a> \r\n</p>');
