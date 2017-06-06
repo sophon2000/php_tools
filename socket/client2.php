0acd689a8953f1ac08bc0f45064ca56769bb5dd5
GeorgeBai123 <xiao12312315@sina.com>
2017-06-06 18:22:37 +0800

    6/6 update

--- socket/client2.php
+++ socket/client2.php
@@ -12,7 +12,7 @@ if ($argv[1]) {
       
     $socket_client = stream_socket_client('tcp://127.0.0.1:2000', $errno, $errstr, 30);  
       
-//  stream_set_timeout($socket_client, 0, 100000);  
+ // stream_set_timeout($socket_client, 0, 100000);  
       
     if (!$socket_client) {  
         die("$errstr ($errno)");  
@@ -22,6 +22,7 @@ if ($argv[1]) {
             $res = fwrite($socket_client, "$msg($i)/n");  
             usleep(100000);  
 //          debug(fread($socket_client, 1024)); // 将产生死锁，因为 fread 在阻塞模式下未读到数据时将等待  
+         // echo fread($socket_client, 1024); 
         }  
         fwrite($socket_client, "quit/n"); // add end token  
         debug(fread($socket_client, 1024));  
@@ -30,15 +31,15 @@ if ($argv[1]) {
 }  
 else {  
       
-    $phArr = array();  
-    for ($i = 0; $i < 10; $i++) {  
-        $phArr[$i] = popen("php ".__FILE__." '{$i}:test'", 'r');  
-    }  
-    foreach ($phArr as $ph) {  
-        pclose($ph);  
-    }  
+    // $phArr = array();  
+    // for ($i = 0; $i < 10; $i++) {  
+    //     $phArr[$i] = popen("php ".__FILE__." '{$i}:test'", 'r');  
+    // }  
+    // foreach ($phArr as $ph) {  
+    //     pclose($ph);  
+    // }  
       
-//  for ($i = 0; $i < 10; $i++) {  
-//      system("php ".__FILE__." '{$i}:test'");  
-//  }  
+ for ($i = 0; $i < 10; $i++) {  
+     system("php ".__FILE__." '{$i}:test'");  
+ }  
 }  
