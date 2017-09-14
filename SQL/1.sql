e8ab318fa5ecfb7f05de97c51f42266121e00211
xxbai <you@example.com>
2017-09-14 16:53:58 +0800

    update 9/14: add some SQL

--- SQL/1.sql
+++ SQL/1.sql
@@ -1,163 +1,295 @@
-SELECT
-	eck_delivery_schedule.id AS id,
-	eck_delivery_schedule.title AS eck_delivery_schedule_title,
-	eck_delivery_schedule.created AS eck_delivery_schedule_created,
-	'delivery_schedule' AS field_data_field_delivery_id_delivery_schedule_entity_type,
-	'delivery_schedule' AS field_data_field_delivery_type_delivery_schedule_entity_type,
-	'delivery_schedule' AS field_data_field_organization_delivery_schedule_entity_type,
-	'delivery_schedule' AS field_data_field_delivery_date_delivery_schedule_entity_type,
-	'delivery_schedule' AS field_data_field_distribution_route_ref_delivery_schedule_en
-FROM
-	{ eck_delivery_schedule } eck_delivery_schedule
-WHERE
-	(
-		(
-			(
-				eck_delivery_schedule.type IN ('delivery_schedule')
-			)
-		)
-	)
-ORDER BY
-	eck_delivery_schedule_created DESC
-LIMIT 30 OFFSET 0
+
+--处理表重复记录(查询和删除)
+1、Num、Name相同的重复值记录,没有大小关系只保留一条
+2、Name相同,ID有大小关系时,保留大或小其中一个记录
+
+--1、用于查询重复处理记录(如果列没有大小关系时2000用生成自增列和临时表处理,SQL2005用row_number函数处理)
+
+if not object_id('Tempdb..#T') is null
+    drop table #T
+Go
+Create table #T([ID] int,[Name] nvarchar(1),[Memo] nvarchar(2))
+Insert #T
+select 1,N'A',N'A1' union all
+select 2,N'A',N'A2' union all
+select 3,N'A',N'A3' union all
+select 4,N'B',N'B1' union all
+select 5,N'B',N'B2'
+Go
 
 
-SELECT
-	field_data_field_delivery_date.delta AS field_data_field_delivery_date_delta,
-	field_data_field_delivery_date.entity_id AS date_id_field_delivery_date,
-	field_data_field_delivery_date.delta AS date_delta_field_delivery_date,
-	eck_delivery_schedule.id AS id,
-	field_data_field_delivery_date. LANGUAGE AS field_data_field_delivery_date_language,
-	field_data_field_delivery_date.bundle AS field_data_field_delivery_date_bundle,
-	field_data_field_delivery_date.field_delivery_date_value AS field_data_field_delivery_date_field_delivery_date_value,
-	field_data_field_delivery_date.field_delivery_date_rrule AS field_data_field_delivery_date_field_delivery_date_rrule,
-	'delivery_schedule' AS field_data_field_delivery_type_delivery_schedule_entity_type,
-	'delivery_schedule' AS field_data_field_delivery_date_offset_delivery_schedule_enti,
-	'delivery_schedule' AS field_data_field_delivery_date_delivery_schedule_entity_type
-FROM
-	{ eck_delivery_schedule } eck_delivery_schedule
-LEFT JOIN { field_data_field_distribution_route_ref } field_data_field_distribution_route_ref ON eck_delivery_schedule.id = field_data_field_distribution_route_ref.entity_id
-AND (
-	field_data_field_distribution_route_ref.entity_type = 'delivery_schedule'
-	AND field_data_field_distribution_route_ref.deleted = '0'
-)
-LEFT JOIN { eck_delivery_schedule } eck_delivery_schedule_field_data_field_distribution_route_ref ON field_data_field_distribution_route_ref.field_distribution_route_ref_target_id = eck_delivery_schedule_field_data_field_distribution_route_ref.id
-LEFT JOIN { field_data_field_delivery_type } field_data_field_delivery_type ON eck_delivery_schedule.id = field_data_field_delivery_type.entity_id
-AND (
-	field_data_field_delivery_type.entity_type = 'delivery_schedule'
-	AND field_data_field_delivery_type.deleted = '0'
-)
-LEFT JOIN { field_data_field_delivery_date } field_data_field_delivery_date ON eck_delivery_schedule.id = field_data_field_delivery_date.entity_id
-AND (
-	field_data_field_delivery_date.entity_type = 'delivery_schedule'
-	AND field_data_field_delivery_date.deleted = '0'
-)
-LEFT JOIN { field_data_field_store_select } eck_delivery_schedule_field_data_field_distribution_route_ref__field_data_field_store_select ON eck_delivery_schedule_field_data_field_distribution_route_ref.id = eck_delivery_schedule_field_data_field_distribution_route_ref__field_data_field_store_select.entity_id
-AND (
-	eck_delivery_schedule_field_data_field_distribution_route_ref__field_data_field_store_select.entity_type = 'delivery_schedule'
-	AND eck_delivery_schedule_field_data_field_distribution_route_ref__field_data_field_store_select.deleted = '0'
-)
-WHERE
-	(
-		(
-			(
-				DATE_FORMAT(
-					STR_TO_DATE(
-						field_data_field_delivery_date.field_delivery_date_value,
-						'%Y-%m-%dT%T'
-					),
-					'%Y-%m'
-				) >= '2017-07'
-				AND DATE_FORMAT(
-					STR_TO_DATE(
-						field_data_field_delivery_date.field_delivery_date_value,
-						'%Y-%m-%dT%T'
-					),
-					'%Y-%m'
-				) <= '2017-07'
-			)
-		)
-		AND (
-			(
-				eck_delivery_schedule_field_data_field_distribution_route_ref__field_data_field_store_select.field_store_select_value LIKE '%CN010001%' ESCAPE '\\'
-			)
-		)
-		AND (
-			(
-				(
-					field_data_field_delivery_type.field_delivery_type_target_id IS NOT NULL
-				)
-				AND (
-					eck_delivery_schedule.erp_workflow_state IN ('published')
-				)
-			)
-		)
-	)
-ORDER BY
-	field_data_field_delivery_date_field_delivery_date_value ASC
+--I、Name相同ID最小的记录(推荐用1,2,3),方法3在SQl05时，效率高于1、2
+方法1:
+Select * from #T a where not exists(select 1 from #T where Name=a.Name and ID<a.ID)
+
+方法2:
+select a.* from #T a join (select min(ID)ID,Name from #T group by Name) b on a.Name=b.Name and a.ID=b.ID
+
+方法3:
+select * from #T a where ID=(select min(ID) from #T where Name=a.Name)
+
+方法4:
+select a.* from #T a join #T b on a.Name=b.Name and a.ID>=b.ID group by a.ID,a.Name,a.Memo having count(1)=1 
+
+方法5:
+select * from #T a group by ID,Name,Memo having ID=(select min(ID)from #T where Name=a.Name)
+
+方法6:
+select * from #T a where (select count(1) from #T where Name=a.Name and ID<a.ID)=0
+
+方法7:
+select * from #T a where ID=(select top 1 ID from #T where Name=a.name order by ID)
+
+方法8:
+select * from #T a where ID!>all(select ID from #T where Name=a.Name)
+
+方法9(注:ID为唯一时可用):
+select * from #T a where ID in(select min(ID) from #T group by Name)
+
+--SQL2005:
+
+方法10:
+select ID,Name,Memo from (select *,min(ID)over(partition by Name) as MinID from #T a)T where ID=MinID
+
+方法11:
+
+select ID,Name,Memo from (select *,row_number()over(partition by Name order by ID) as MinID from #T a)T where MinID=1
+
+生成结果:
+/*
+ID          Name Memo
+----------- ---- ----
+1           A    A1
+4           B    B1
+
+(2 行受影响)
+*/
+
+
+--II、Name相同ID最大的记录,与min相反:
+方法1:
+Select * from #T a where not exists(select 1 from #T where Name=a.Name and ID>a.ID)
+
+方法2:
+select a.* from #T a join (select max(ID)ID,Name from #T group by Name) b on a.Name=b.Name and a.ID=b.ID order by ID
+
+方法3:
+select * from #T a where ID=(select max(ID) from #T where Name=a.Name) order by ID
+
+方法4:
+select a.* from #T a join #T b on a.Name=b.Name and a.ID<=b.ID group by a.ID,a.Name,a.Memo having count(1)=1 
+
+方法5:
+select * from #T a group by ID,Name,Memo having ID=(select max(ID)from #T where Name=a.Name)
+
+方法6:
+select * from #T a where (select count(1) from #T where Name=a.Name and ID>a.ID)=0
+
+方法7:
+select * from #T a where ID=(select top 1 ID from #T where Name=a.name order by ID desc)
+
+方法8:
+select * from #T a where ID!<all(select ID from #T where Name=a.Name)
+
+方法9(注:ID为唯一时可用):
+select * from #T a where ID in(select max(ID) from #T group by Name)
+
+--SQL2005:
+
+方法10:
+select ID,Name,Memo from (select *,max(ID)over(partition by Name) as MinID from #T a)T where ID=MinID
+
+方法11:
+select ID,Name,Memo from (select *,row_number()over(partition by Name order by ID desc) as MinID from #T a)T where MinID=1
+
+生成结果2:
+/*
+ID          Name Memo
+----------- ---- ----
+3           A    A3
+5           B    B2
+
+(2 行受影响)
+*/
 
 
 
-	SELECT * FROM 
-			   (SELECT SUM(kills) AS '沙僧' FROM user1 a JOIN user_kills b ON a.id=b.user_id AND a.user_name='沙僧') a
-	CROSS JOIN (SELECT SUM(kills) as '猪八戒' FROM user1 a JOIN user_kills b ON a.id=b.user_id AND a.user_name='猪八戒') b
-	CROSS JOIN (SELECT SUM(kills) as '送悟空' FROM user1 a JOIN user_kills b ON a.id=b.user_id AND a.user_name='送悟空') b
+--2、删除重复记录有大小关系时,保留大或小其中一个记录
 
-	
-SELECT
-	id AS id,
-	subsidy_fine_date AS hr_subsidy_fine_subsidy_fine_date,
-	subsidy_fine_type AS hr_subsidy_fine_subsidy_fine_type,
-	employee_name AS hr_subsidy_fine_employee_name,
-	employee_id AS hr_subsidy_fine_employee_id,
-	making_date AS hr_subsidy_fine_making_date
-FROM
-	 hr_subsidy_fine
-WHERE
-	(
-		(
-			(
-				institutions_id = 'CN010001'
-			)
-		)
-		AND (
-			(
-				(
-					subsidy_fine_date BETWEEN '1498838400'
-					AND '1501516800'
-				)
-			)
-		)
-	)
-ORDER BY
-	hr_subsidy_fine_making_date DESC
 
-	USE erp;
+--> --> (Roy)生成測試數據
 
-SELECT
-	otherincomedate.bundle,
-	#模块
-	eck_cash.created,
-	#创建日期
-	eck_cash.store_id,
-	#业务数据门店ID
-	eck_cash.note,
-	#备注
-	otherincomeitem.field_otherincomeitem_tid AS otherincomeitem_tid,
-	#其他业务收入项目ID
-	otherincomemethod.field_otherincomemethod_tid AS paytype_tid,
-	#收款方式ID
-	otherincomedate.field_otherincomedate_value AS business_date,
-	#业务日期
-	eck_cash.amount,
-	#金额
-	otherincomequanlity.field_otherincomequanlity_value AS quanlity #数量
-FROM
-	field_data_field_otherincomedate AS otherincomedate
-LEFT JOIN eck_cash_manage AS eck_cash ON otherincomedate.entity_id = eck_cash.id
-LEFT JOIN field_data_field_otherincomemethod AS otherincomemethod ON otherincomedate.entity_id = otherincomemethod.entity_id
-LEFT JOIN field_data_field_otherincomequanlity AS otherincomequanlity ON otherincomedate.entity_id = otherincomequanlity.entity_id
-LEFT JOIN field_data_field_otherincomeitem AS otherincomeitem ON otherincomedate.entity_id = otherincomeitem.entity_id
-WHERE
-	eck_cash.store_id IN ('CN010003')
-AND eck_cash.created = '2017-07-20'
+if not object_id('Tempdb..#T') is null
+    drop table #T
+Go
+Create table #T([ID] int,[Name] nvarchar(1),[Memo] nvarchar(2))
+Insert #T
+select 1,N'A',N'A1' union all
+select 2,N'A',N'A2' union all
+select 3,N'A',N'A3' union all
+select 4,N'B',N'B1' union all
+select 5,N'B',N'B2'
+Go
+
+--I、Name相同ID最小的记录(推荐用1,2,3),保留最小一条
+方法1:
+delete a from #T a where  exists(select 1 from #T where Name=a.Name and ID<a.ID)
+
+方法2:
+delete a  from #T a left join (select min(ID)ID,Name from #T group by Name) b on a.Name=b.Name and a.ID=b.ID where b.Id is null
+
+方法3:
+delete a from #T a where ID not in (select min(ID) from #T where Name=a.Name)
+
+方法4(注:ID为唯一时可用):
+delete a from #T a where ID not in(select min(ID)from #T group by Name)
+
+方法5:
+delete a from #T a where (select count(1) from #T where Name=a.Name and ID<a.ID)>0
+
+方法6:
+delete a from #T a where ID<>(select top 1 ID from #T where Name=a.name order by ID)
+
+方法7:
+delete a from #T a where ID>any(select ID from #T where Name=a.Name)
+
+
+
+select * from #T
+
+生成结果:
+/*
+ID          Name Memo
+----------- ---- ----
+1           A    A1
+4           B    B1
+
+(2 行受影响)
+*/
+
+
+--II、Name相同ID保留最大的一条记录:
+
+方法1:
+delete a from #T a where  exists(select 1 from #T where Name=a.Name and ID>a.ID)
+
+方法2:
+delete a  from #T a left join (select max(ID)ID,Name from #T group by Name) b on a.Name=b.Name and a.ID=b.ID where b.Id is null
+
+方法3:
+delete a from #T a where ID not in (select max(ID) from #T where Name=a.Name)
+
+方法4(注:ID为唯一时可用):
+delete a from #T a where ID not in(select max(ID)from #T group by Name)
+
+方法5:
+delete a from #T a where (select count(1) from #T where Name=a.Name and ID>a.ID)>0
+
+方法6:
+delete a from #T a where ID<>(select top 1 ID from #T where Name=a.name order by ID desc)
+
+方法7:
+delete a from #T a where ID<any(select ID from #T where Name=a.Name)
+
+
+select * from #T
+/*
+ID          Name Memo
+----------- ---- ----
+3           A    A3
+5           B    B2
+
+(2 行受影响)
+*/
+
+
+
+
+
+--3、删除重复记录没有大小关系时，处理重复值
+
+
+--> --> (Roy)生成測試數據
+ 
+if not object_id('Tempdb..#T') is null
+    drop table #T
+Go
+Create table #T([Num] int,[Name] nvarchar(1))
+Insert #T
+select 1,N'A' union all
+select 1,N'A' union all
+select 1,N'A' union all
+select 2,N'B' union all
+select 2,N'B'
+Go
+
+方法1:
+if object_id('Tempdb..#') is not null
+    drop table #
+Select distinct * into # from #T--排除重复记录结果集生成临时表#
+
+truncate table #T--清空表
+
+insert #T select * from #    --把临时表#插入到表#T中
+
+--查看结果
+select * from #T
+
+/*
+Num         Name
+----------- ----
+1           A
+2           B
+
+(2 行受影响)
+*/
+
+--重新执行测试数据后用方法2
+方法2:
+
+alter table #T add ID int identity--新增标识列
+go
+delete a from  #T a where  exists(select 1 from #T where Num=a.Num and Name=a.Name and ID>a.ID)--只保留一条记录
+go
+alter table #T drop column ID--删除标识列
+
+--查看结果
+select * from #T
+
+/*
+Num         Name
+----------- ----
+1           A
+2           B
+
+(2 行受影响)
+
+*/
+
+--重新执行测试数据后用方法3
+方法3:
+declare Roy_Cursor cursor local for
+select count(1)-1,Num,Name from #T group by Num,Name having count(1)>1
+declare @con int,@Num int,@Name nvarchar(1)
+open Roy_Cursor
+fetch next from Roy_Cursor into @con,@Num,@Name
+while @@Fetch_status=0
+begin 
+    set rowcount @con;
+    delete #T where Num=@Num and Name=@Name
+    set rowcount 0;
+    fetch next from Roy_Cursor into @con,@Num,@Name
+end
+close Roy_Cursor
+deallocate Roy_Cursor
+
+--查看结果
+select * from #T
+/*
+Num         Name
+----------- ----
+1           A
+2           B
+
+(2 行受影响)
+*/
