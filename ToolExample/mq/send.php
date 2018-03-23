024c09459ad54337dbbf873a3d197f2dc919dfed
baixiaoxiao <you@example.com>
2018-03-23 15:20:59 +0800

    3.23 update

---
+++ ToolExample/mq/send.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+
+$connection = new AMQPStreamConnection('mq', 5672, 'zfm', '123456','my_vhost');
+$channel = $connection->channel();
+$channel->queue_declare('hello', false, false, false, false);
+$msg = new AMQPMessage('Hello World!');
+$channel->basic_publish($msg, '', 'hello');
+echo " [x] Sent 'Hello World!'\n";
+
+$channel->close();
+
+$connection->close();
+
+
+
+?>
